65d41130d8e0893b5b94008984d6526e867c99aa
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-04-30 14:43:13 -0600

    Objetos y propiedades computadas

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,3 +1,4 @@
+import { useShallow } from "zustand/react/shallow";
 import { WhiteCard } from "../../components";
 import { useBearStore } from "../../stores";
 
@@ -12,6 +13,7 @@ export const BearPage = () => {
         <BlackBears />
         <PolarBears />
         <PandaBears />
+        <BearsDisplay />
       </div>
     </>
   );
@@ -65,6 +67,27 @@ export const PandaBears = () => {
         <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
         <button onClick={() => increasePandaBears(-1)}>-1</button>
       </div>
+    </WhiteCard>
+  );
+};
+
+export const BearsDisplay = () => {
+  // useShallow verifica si el estado cambia para evitar los re-render
+  const bears = useBearStore(useShallow((state) => state.bears));
+  const doNothing = useBearStore((state) => state.doNothing);
+  const addBear = useBearStore((state) => state.addBear);
+  const clearBears = useBearStore((state) => state.clearBears);
+  return (
+    <WhiteCard>
+      <h1>Osos</h1>
+      <button onClick={doNothing}>No hace nada</button>
+      <button className="mt-2" onClick={addBear}>
+        Agregar oso
+      </button>
+      <button className="mt-2" onClick={clearBears}>
+        Borrar osos
+      </button>
+      <pre>{JSON.stringify(bears, null, 2)}</pre>
     </WhiteCard>
   );
 };
